f60b561cd16a262648f15c4bbd86eba98a8caa9f
dmchoi <david5760@naver.com>
2022-05-15 22:38:29 +0900

    Add - modal 버튼, 기능 & clipboard 복사

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 
 function Content() {
-  return <ContentWrapper></ContentWrapper>;
+  const [modal, setModal] = useState(false);
+
+  function copyUrlToClipBoard() {
+    // 클립보드 복사를 위해 Clipboard API 사용
+    // 참고 https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/writeText
+    navigator.clipboard.writeText(window.location.href).then(() => {
+      alert("클립보드에 복사되었습니다.");
+    });
+  }
+
+  function clickModal() {
+    setModal((value) => !value);
+  }
+
+  return (
+    <ContentWrapper>
+      {modal ? (
+        <Modal onClick={clickModal}>
+          <ModalContent src="/images/manual.png" />
+        </Modal>
+      ) : null}
+      <ViewerWrapper>
+        <ModalIcon onClick={clickModal}>ℹ️</ModalIcon>
+        <Viewer>Viewer</Viewer>
+      </ViewerWrapper>
+      <CopyButton
+        type="button"
+        value="코드 복사하기"
+        onClick={copyUrlToClipBoard}
+      />
+    </ContentWrapper>
+  );
 }
 
 export default Content;
@@ -12,9 +43,53 @@ const ContentWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: clamp(640px, 43vh, 1000px);
   background-image: url(/images/background.png);
   background-size: cover;
   background-position: center center;
   background-repeat: no-repeat;
+`;
+const ViewerWrapper = styled.div`
+  margin-top: 20px;
+  width: 720px;
+  height: 440px;
+`;
+
+const Viewer = styled.div`
+  width: 100%;
+  height: 380px;
+  // display: block;
+  position: relative;
+  overflow: hidden;
+  background-color: #ffff;
+  border-radius: 4px;
+`;
+
+const ModalIcon = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  cursor: pointer;
+`;
+
+const CopyButton = styled.input`
+  background-color: #0071ff;
+  color: #ffff;
+  border: transparent;
+  cursor: pointer;
+  border-radius: 4px;
+  padding: 8px;
+  font-size: 14px;
+  margin-bottom: 35px;
+`;
+
+const Modal = styled.div`
+  background-color: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  inset: 0px;
+  z-index: 50;
+  cursor: pointer;
+`;
+
+const ModalContent = styled.img`
+  display: flex;
+  margin: 400px auto;
 `;
