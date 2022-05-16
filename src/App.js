cb1c7dbd7b32e6d81a34d90edbb6a5dad3e4a4da
dmchoi <david5760@naver.com>
2022-05-16 21:17:06 +0900

    Fix - modified components path

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import GlobalStyle from "./Common/GlobalStyle";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
-import SubContent from "./components/SubContent";
+import Header from "./Common/Header";
+import MainContent from "./Components/MainContent";
+import Footer from "./Common/Footer";
+import SubContent from "./Components/SubContent";
 
 function App() {
   return (
