e6bc9c5b4889ca86449c3f79c240fc07a75d3e96
dmchoi <david5760@naver.com>
2022-06-08 11:22:24 +0900

    design(GlobalStyle): width, height 설정

--- src/Common/GlobalStyle.js
+++ src/Common/GlobalStyle.js
@@ -2,7 +2,12 @@ import React from "react";
 import { Global, css } from "@emotion/react";
 
 const defaultStyle = css`
-  * {
+  body {
+    min-width: 768px;
+    max-width: 1920px;
+
+    min-height: 1024px;
+    max-height: 1280px;
   }
 `;
 
